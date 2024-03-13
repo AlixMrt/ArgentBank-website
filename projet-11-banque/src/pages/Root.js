38778e68a5e06392b4979fde948e2bedacca4c46
Alix Maret <alix.mrt@proton.me>
2024-03-13 20:53:01 +0100

    added the SignIn and User pages

--- projet-11-banque/src/pages/Root.js
+++ projet-11-banque/src/pages/Root.js
@@ -7,9 +7,7 @@ export default function RootLayout() {
   return (
     <>
       <Navbar />
-      <main className="main">
-        <Outlet />
-      </main>
+      <Outlet />
       <Footer />
     </>
   );
