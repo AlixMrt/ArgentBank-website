2cd0795a4765765ce1185df127e149397d77aa04
Alix Maret <alix.mrt@proton.me>
2024-03-25 16:08:06 +0100

    added two components EditUser and Transactions and implemented them in the User.js page

--- projet-11-banque/src/pages/SignIn.js
+++ projet-11-banque/src/pages/SignIn.js
@@ -1,31 +1,64 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../store/UserSlice";
 
 export default function SignInPage() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleLoginEvent = (e) => {
+    e.preventDefault();
+    let userCredentials = {
+      userName,
+      password,
+      rememberMe,
+    };
+    dispatch(loginUser(userCredentials))
+  };
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form onSubmit={handleLoginEvent}>
           <div className="input-wrapper">
-            <label for="username">Username</label>
-            <input type="text" id="username" />
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              required
+            />
           </div>
           <div className="input-wrapper">
-            <label for="password">Password</label>
-            <input type="password" id="password" />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
+            <input
+              type="checkbox"
+              id="remember-me"
+              value={rememberMe}
+              onChange={(e) => setRememberMe(e.target.value)}
+            />
+            <label htmlFor="remember-me">Remember me</label>
           </div>
           {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-          <a href="./user.html" className="sign-in-button">
+          <Link to="/user" className="sign-in-button">
             Sign In
-          </a>
-          {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
-          {/* <!-- <button className="sign-in-button">Sign In</button> --> */}
-          {/* <!--  --> */}
+          </Link>
         </form>
       </section>
     </main>
